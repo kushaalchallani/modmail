b3c23478b913bee9e27cc1b085b435672aecb68b
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-15 12:58:50 +0530

    Create verify.js

---
+++ src/commands/verify.js
@@ -0,0 +1,28 @@
+const BaseCommand = require('.././utils/structures/BaseCommand');
+const Discord = require("discord.js");
+
+module.exports = class extends BaseCommand {
+  constructor() {
+    super('verify', '', []);
+  }
+
+  async run(client, message, args) {
+    if (message.member.roles.cache.has('707197658007339069')) {
+        const embed = new MessageEmbed()
+        .setTitle("You are already Verified")
+        .setColor(color.red_light)
+        .setDescription("You already are verified the server, You cannot be verified again")
+        .setTimestamp()
+    return message.channel.send(embed).then(m => m.delete({ timeout: 5000}));
+        
+    }
+
+   if (message.member.roles.add('707197658007339069')) {
+    const done = new MessageEmbed()
+        .setTitle("You are Verified")
+        .setColor(color.green_light)
+        .setDescription("You are verified the server, You may now have access to the channels")
+        .setTimestamp()
+       return message.channel.send(done).then(m => m.delete({ timeout: 2500}));}
+  }
+  }
