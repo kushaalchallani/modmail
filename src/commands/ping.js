ff97995493acf8f3cdf9286e4483915162b2eda6
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-15 12:44:54 +0530

    Update ping.js

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -7,17 +7,21 @@ module.exports = class ping extends BaseCommand {
   }
 
   async run(client, message, args) {
-    message.channel.send(`🏓 Pinging....`).then((msg) => {
-      const _ = new Discord.MessageEmbed()
-        .setTitle("Pong!")
-        .setDescription(
-          `🏓 Pong!\nLatency is ${Math.floor(
-            msg.createdTimestamp - message.createdTimestamp
-          )}ms\nAPI Latency is ${Math.round(this.client.ws.ping)}ms`
-        )
-        .setColor("BLUE");
-      msg.edit(_);
-      msg.edit("\u200B");
-    });
+    message.channel.send(`
+    🏓
+     Pinging....`).then((msg) => {
+          const _ = new Discord.MessageEmbed()
+            .setTitle("Pong!")
+            .setDescription(
+              `
+    🏓
+     Pong!\nLatency is ${Math.floor(
+                msg.createdTimestamp - message.createdTimestamp
+              )}ms\nAPI Latency is ${Math.round(bot.ws.ping)}ms`
+            )
+            .setColor("RANDOM");
+          msg.edit(_);
+          msg.edit("\u200B");
+        });
   }
   }
