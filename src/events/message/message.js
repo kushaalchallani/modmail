464a3e44bfc7d854adbed00fe4a651de6d8fbec7
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-15 13:19:16 +0530

    Update message.js

--- src/events/message/message.js
+++ src/events/message/message.js
@@ -6,6 +6,18 @@ module.exports = class MessageEvent extends BaseEvent {
   }
   
   async run(client, message) {
+    const mentionRegex = RegExp(`^<@!${this.client.user.id}>$`);
+        const mentionRegexPrefix = RegExp(`^<@!${this.client.user.id}> `);
+
+        if (!message.guild || message.author.bot) return;
+
+        if (message.content.match(mentionRegex)) message.channel.send(`My prefix for ${message.guild.name} is **\`${this.client.prefix}\`**.\nType **\`${this.client.prefix}help\`** for more info`);
+
+        const prefix = message.content.match(mentionRegexPrefix) ?
+            message.content.match(mentionRegexPrefix)[0] : this.client.prefix;
+
+        if(!message.content.startsWith(prefix)) return;
+        
     if (message.author.bot) return;
     if(message.channel.type === 'dm') {
       console.log('Inside message Event')
