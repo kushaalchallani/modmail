f965cf77fe375efc80857353820fa037d01fd80f
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-15 11:55:38 +0530

    10

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -2,10 +2,10 @@ const BaseCommand = require('.././utils/structures/BaseCommand');
 
 module.exports = class TestCommand extends BaseCommand {
   constructor() {
-    super('test', 'testing', []);
+    super('ping', '', []);
   }
 
   async run(client, message, args) {
-    message.channel.send('pong!');
+    message.channel.send('Pong!');
   }
 }
