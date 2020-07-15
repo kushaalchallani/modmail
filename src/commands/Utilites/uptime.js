df7bc749550637967aa9b0e5f080820b75d40b03
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-15 13:39:36 +0530

    Update uptime.js

--- src/commands/Utilites/uptime.js
+++ src/commands/Utilites/uptime.js
@@ -5,14 +5,15 @@ const color = require("../../events/message/colors.json")
 
 module.exports = class extends BaseCommand {
   constructor() {
-    super('verify', '', []);
+    super('uptime', '', []);
   }
 
   async run(client, message, args) {
+
     const embed  = new MessageEmbed()
     .setColor(color.blue_light)
     .setTitle("Uptime")
-    .setDescription(`My uptime is \`${ms(this.client.uptime, {long: true })}\``)
+    .setDescription(`My uptime is \`${ms(client.uptime, {long: true })}\``)
     
 
     message.channel.send(embed);
