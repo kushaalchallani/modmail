e0eaf83d3a447e627d2550d99d25cf00b231ea1f
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-16 15:27:53 +0530

    Update ready.js

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -9,14 +9,7 @@ module.exports = class ReadyEvent extends BaseEvent {
     //client.user.setActivity("DM to Contact Staff", {type: "PLAYING"});
     console.log(client.user.tag + ' is now online.');
 
-    let statuses = [
-      `over ${client.users.cache.size} users!`,
-      `#🌐┃global-chat`,
-      `DM to Contact Staff`,
-    ]
-
     setInterval(function(){
-      let status = statuses[Math.floor(Math.random() * statuses.length)]
       client.user.setActivity("lol", { type: "WATCHING"})
       client.user.setActivity("lol2", { type: "PLAYING"})
   }, 20000)
