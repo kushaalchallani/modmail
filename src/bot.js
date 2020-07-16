6a44d3c9497de45409493d54ad5e03a3c7841530
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-16 09:52:01 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -4,6 +4,7 @@ const { registerCommands, registerEvents } = require('./utils/registry');
 const client = new Client();
 
 (async (message) => {
+  client.afk = new Map();
   client.commands = new Map();
   client.events = new Map();
   client.prefix = process.env.DISCORD_BOT_PREFIX;
@@ -12,10 +13,10 @@ const client = new Client();
   await client.login(process.env.token);
 
   if (message.content.includes(message.mentions.members.first())) {
-    let mentioned = bot.afk.get(message.mentions.users.first().id);
+    let mentioned = client.afk.get(message.mentions.users.first().id);
     if (mentioned) message.channel.send(`**${mentioned.usertag}** is currently afk. Reason: ${mentioned.reason}`);
   }
-  let afkcheck = bot.afk.get(message.author.id);
-  if (afkcheck) return [bot.afk.delete(message.author.id), message.reply(`you have been removed from the afk list!`).then(msg => msg.delete(5000))];
+  let afkcheck = client.afk.get(message.author.id);
+  if (afkcheck) return [client.afk.delete(message.author.id), message.reply(`you have been removed from the afk list!`).then(msg => msg.delete(5000))];
 })();
 
