b5b6e54eb499b50f5fbe910039c48f0f741cb818
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-16 16:36:56 +0530

    Update ready.js

--- src/events/ready/ready.js
+++ src/events/ready/ready.js
@@ -8,7 +8,16 @@ module.exports = class ReadyEvent extends BaseEvent {
     async run (client) {
     console.log(client.user.tag + ' is now online.');
 
-    let statuses = [ {name: "lol", options: { type: "WATCHING" }}, { name: "lol2", options: { type: "PLAYING" }}]
+    let statuses = [ 
+      {name: "KC's Universe", options: { type: "WATCHING" }}, 
+      { name: "DM to contact staff", options: { type: "PLAYING" }},
+      { name: "Valorant", options: { type: "PLAYING" }},
+      { name: "KC Playz's Videos", options: { type: "WATCHING" }},
+      { name: "Gogeta", options: { type: "LISTENING" }},
+      { name: "🌐┃global-chat", options: { type: "STREAMING", URL: 'https://www.youtube.com/c/kcplayz' }},
+      { name: "Rainbow 6 Siege", options: { type: "PLAYING" }},
+
+    ]
 
     setInterval(() => {
       const status = statuses[Math.floor(Math.random() * statuses.length)];
