a1d837d964cb2f28161ef8b5f04a8b153530bb0f
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-17 14:17:09 +0530

    26

--- src/bot.js
+++ src/bot.js
@@ -20,20 +20,6 @@ const LIMIT = 6;
 const TIME = 10000;
 const DIFF = 3000;
 const UNMUTE = 10800000;
-const REMOVE = ([
-  '707197658007339069',
-  '707133063741702166',
-  '707133501354541126',
-  '708620116291485758',
-  '708620116291485758',
-  '717660704617791520',
-  '717660767922683932',
-  '707134101722890250',
-  '707630691944366151',
-  '717728401334599690',
-  '707904015412756493',
-  '708591193461882880',
-])
 
 client.on('message', message => {
   if(message.author.bot) return;
@@ -103,18 +89,6 @@ if(!message.member.hasPermission('MANAGE_MESSAGES')){
     setTimeout(async() => {
       message.member.roles.remove(role2)
     }, ms('5m'))
-  }
-}
-
-var array2 = ['nigga', 'nigger', 'niggah'];
-
-if(!message.member.hasPermission('MANAGE_MESSAGES')){
-  if(array2.some(w2 => ` ${message.content.toLowerCase()} `.includes(` ${w2}`))){
-    message.delete();
-
-    var role2 = message.guild.roles.cache.get('715107146127114321');
-
-    message.member.ban()
   }
 }
 
---
+++ src/commands/Moderation/mute.js
@@ -0,0 +1,67 @@
+const BaseCommand = require('../../utils/structures/BaseCommand');
+const Discord = require("discord.js");
+const color = require("../../events/message/colors.json")
+const ms = require('ms');
+
+module.exports = class extends BaseCommand {
+  constructor() {
+    super('mute', 'Mutes a user in the server', ['m', 'nospeak', 'notalk']);
+  }
+
+  async run(client, message, args) {        
+    if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('You can\'t use that!');
+
+    var user = message.mentions.users.first();
+    if(!user) return message.reply('You didn\'t mention anyone!');
+
+    var member;
+
+    try {
+        member = await message.guild.members.fetch(user);
+    } catch(err) {
+        member = null;
+    }
+
+    if(!member) return message.reply('They aren\'t in the server!');
+    if(member.hasPermission('MANAGE_MESSAGES')) return message.reply('You cannot mute that person!');
+
+    var rawTime = args[1];
+    var time = ms(rawTime);
+    if(!time) return message.reply('You didn\'t specify a time!');
+
+    var reason = args.splice(2).join(' ');
+    if(!reason) return message.reply('You need to give a reason!');
+
+    var channel = message.guild.channels.cache.find(c => c.name === 'potato');
+
+    var log = new Discord.MessageEmbed()
+    .setTitle('User Muted')
+    .addField('User:', user, true)
+    .addField('By:', message.author, true)
+    .addField('Expires:', rawTime)
+    .addField('Reason:', reason)
+    channel.send(log);
+
+    var embed = new Discord.MessageEmbed()
+    .setTitle('You were muted!')
+    .addField('Expires:', rawTime, true)
+    .addField('Reason:', reason, true);
+
+    try {
+        user.send(embed);
+    } catch(err) {
+        console.warn(err);
+    }
+
+    var role = message.guild.roles.cache.find(r => r.name === 'Muted');
+
+    member.roles.add(role);
+
+    setTimeout(async() => {
+        member.roles.remove(role);
+    }, time);
+
+    message.channel.send(`**${user}** has been muted by **${message.author}** for **${rawTime}**!`);
+    
+  }
+  }
