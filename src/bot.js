c22296132a035c356bc2cd3087c63a669fa8aa71
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-18 20:09:32 +0530

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -3,7 +3,6 @@ const { Client, MessageAttachment } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const client = new Client();
 const ms = require('ms');
-const mongoose = require('mongoose')
 
 (async () => {
   client.commands = new Map();
@@ -13,6 +12,8 @@ const mongoose = require('mongoose')
   await registerEvents(client, '../events');
   await client.login(process.env.token);
 })();
+
+const mongoose = require('mongoose')
 
 mongoose.connect("mongodb+srv://kcplayz:rakshaa02@cluster0.k0uop.mongodb.net/Data?retryWrites=true&w=majority",{
   useUnifiedTopology: true,
