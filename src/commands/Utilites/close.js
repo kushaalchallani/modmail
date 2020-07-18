5b8fd46e044be116f133929277b73f87ebd4c23e
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-18 18:31:58 +0530

    Update close.js

--- src/commands/Utilites/close.js
+++ src/commands/Utilites/close.js
@@ -11,13 +11,16 @@ module.exports = class extends BaseCommand {
       if (!message.member.hasPermission('MANAGE_MESSAGES', 'ADMINISTRATOR')){
         return  message.channel.send("You cannot do that")
     }
+
+    message.delete({ timeout: 5000 });
+
     const embed = new Discord.MessageEmbed()
     .setTitle('Mail Closed')
     .setDescription('Mail has been closed.')
     .setTimestamp()
     .setFooter("KC's Universe | 582182796626493444", message.client.guilds.cache.get("582182796626493444").iconURL({ dynamic: true, format: 'png' }))
-    message.channel.send(embed).then(m => m.delete(5000));
-
+    message.channel.send(embed)
+    
     message.channel.bulkDelete(100, true)
     .catch(err => message.channel.send(`Something went wrong... ${err}`));
   }
