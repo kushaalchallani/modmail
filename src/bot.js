aca45ab013003e37d7b549604770e2c474689717
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-18 20:13:15 +0530

    35

--- src/bot.js
+++ src/bot.js
@@ -3,13 +3,6 @@ const { Client, MessageAttachment } = require('discord.js');
 const { registerCommands, registerEvents } = require('./utils/registry');
 const client = new Client();
 const ms = require('ms');
-const mongoose = require('mongoose')
-
-mongoose.connect("mongodb+srv://kcplayz:rakshaa02@cluster0.k0uop.mongodb.net/Data?retryWrites=true&w=majority",{
-  useUnifiedTopology: true,
-  useNewUrlParser: true,
-})
-
 
 (async () => {
   client.commands = new Map();
