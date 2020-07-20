189e405b18d2a8250a8bb4269ab04d03ca4c9064
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-20 08:39:33 +0530

    Update clear.js

--- src/commands/Moderation/clear.js
+++ src/commands/Moderation/clear.js
@@ -10,23 +10,29 @@ module.exports = class extends BaseCommand {
   }
 
   async run(client, message, args) {
-    
-    const messageArray = message.content.split(' ');
-    const args = messageArray.slice(1);
-  
-      if (!message.member.permissions.has("MANAGE_MESSAGES")) return message.channel.send('Lack of Perms!');
-      
-      let deleteAmount;
-  
-      if (isNaN(args[0]) || parseInt(args[0]) <= 0) { return message.reply('Please put a number only!') }
-  
-      if (parseInt(args[0]) > 100) {
-          return message.reply('You can only delete 100 messages at a time!')
-      } else {
-          deleteAmount = parseInt(args[0]);
-      }
-  
-      message.channel.bulkDelete(deleteAmount + 1, true);
-      message.reply(`**Successfully** Deleted ***${deleteAmount}*** Messages.`)
+    if (message.deletable) {
+        message.delete();
+    }
+
+    if (!message.member.hasPermission("MANAGE_MESSAGES")) {
+        return  message.channel.send("You cannot do that").then(m => m.delete(5000));
+    }
+
+    if (isNaN(args[0]) || parseInt(args[0]) <= 0) {
+        return  message.channel.send("This is not a number").then(m => m.delete({ timeout: 5000}));
+    }
+
+    let deleteAmount;
+    if (parseInt(args[0]) > 100) {
+        deleteAmount = 100;
+    } else {
+        deleteAmount = parseInt(args[0]);
+    }
+
+
+    message.channel.bulkDelete(deleteAmount, true)
+    .catch(err => message.channel.send(`Something went wrong... ${err}`));
+
+    message.channel.send(`I have cleared ${deleteAmount} messages`)
   }
   }
