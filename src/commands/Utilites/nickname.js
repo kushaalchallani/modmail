123d4b050c32f46192d6cf1832d44a003acd2ad5
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-20 08:54:31 +0530

    Update nickname.js

--- src/commands/Utilites/nickname.js
+++ src/commands/Utilites/nickname.js
@@ -13,7 +13,7 @@ module.exports = class extends BaseCommand {
     return message.channel.send({embed: {color: "RED", description: "You can't use this command!"}})
   }
   
-  let user = message.mentions.users.first(); // You can mention someone, not only just user.
+  let user = message.mentions.users.first() ||  message.guild.members.cache.get(args[0]);
   if (!user) return message.channel.send({embed: {color: "RED", description: "You need to mention the user!"}});
   
   let nick = args.slice(1).join(" ");
@@ -22,6 +22,6 @@ module.exports = class extends BaseCommand {
   let member = message.guild.members.cache.get(user.id);
   
   await member.setNickname(nick).catch(err => message.channel.send({embed: {color: "RED", description: `Error: ${err}`}}));
-  return message.channel.send({embed: {color: "GREEN", description: `Successfully changed **${user.tag}** nickname to **${nick}**`}});
+  return message.channel.send({embed: {color: "GREEN", description: `<a:tick:733258832456843275> Successfully changed **${user.tag}** nickname to **${nick}**`}});
   }
   }
