b0b2cbba083de98a6c8eaa15d37a17c5c1107edb
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-20 09:49:49 +0530

    Update slowmode.js

--- src/commands/Moderation/slowmode.js
+++ src/commands/Moderation/slowmode.js
@@ -11,6 +11,8 @@ module.exports = class extends BaseCommand {
 
   async run(client, message, args) {
     message.delete()
+    if (!message.member.hasPermission(["MANAGE_CHANNELS", "ADMINISTRATOR"])) return message.channel.send("You do not have permissions to perform this command");
+
     if (!message.member.permissions.any(["ADMINISTRATOR", "MANAGE_CHANNELS"])) {
       return message.channel.send("Oopsie, you don't have any rights to do this.");
     }
@@ -21,30 +23,30 @@ module.exports = class extends BaseCommand {
     if (!channel) time = args.join(" "), channel = message.channel;
     // If the user doesn't includes the channel.
     
-    if (!message.flags[0] === "off") {
+    if (message.flags[0] === "off") {
       channel.setRateLimitPerUser(0);
       return message.channel.send(`<#${channel.id}> slowmode has been deactivated.`);
     }
     
-    if (!time) return message.channel.send("Please includes the time format.");
-    
-    let convert = ms(time); // This will results the milliseconds.
-    let toSecond = Math.floor(convert / 1000); // This will convert the ms to s. (seconds)
-    
-    if (!toSecond || toSecond == undefined) return message.channel.send("Please insert the valid time format!");
-    
-    if (toSecond > 21600) return message.channel.send("Timer should be less than or equal to 6 hours.");
-    else if (toSecond < 1) return message.channel.send("Timer should be more than or equal to 1 second.");
-    
-    await channel.setRateLimitPerUser(toSecond);
-    return message.channel.send(`This channel: <#${channel.id}> has been slowing down for **${ms(ms(time), {long: true})}**.`);
+    if (!args[0])
+    return message.channel.send(
+      `You did not specify the time in seconds you wish to set this channel's slow mode too!`
+    );
+  if (isNaN(args[0])) return message.channel.send(`That is not a number!`);
+  let reason = message.content.slice(
+    bot.prefix.length + 9 + args[0].length + 1
+  );
+  if (!reason) {
+    reason == "No reason provided!";
+  }
+  message.channel.setRateLimitPerUser(args[0], reason);
 
-      //const embed = new MessageEmbed()
-      //.setColor('GREEN')
-      //.setTitle('Slowmode')
-      //.setDescription(`<a:tick:733258832456843275> Set the slowmode of this channel to **${args[0]}** with the reason: **${reason}**`)
-      //.setFooter(`Done by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-      //.setTimestamp()
+      const embed = new MessageEmbed()
+      .setColor('GREEN')
+      .setTitle('Slowmode')
+      .setDescription(`<a:tick:733258832456843275> Set the slowmode of this channel to **${args[0]}** with the reason: **${reason}**`)
+      .setFooter(`Done by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
 
       message.channel.send(embed)
   }
