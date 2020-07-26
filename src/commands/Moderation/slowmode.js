cf5751a2e7e45c7fa78a0321e4a07a06bd737e58
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-26 18:02:02 +0530

    Update slowmode.js

--- src/commands/Moderation/slowmode.js
+++ src/commands/Moderation/slowmode.js
@@ -12,6 +12,18 @@ module.exports = class extends BaseCommand {
   async run(client, message, args) {
 
     message.delete()
+
+    const filter = (m) => m.author.id === message.author.id && (m.content.toLowerCase() === "yes" || m.content.toLowerCase() === "no")  // Create a filter, only accept messages from the user that used the command and the message includes "yes" or "no"
+    message.channel.awaitMessages(filter, {max: 1, time: 30000})
+    .then(collected => {
+        const msg = collected.first()
+        if(msg.content.toLowerCase() === "yes") {
+            message.channel.send(embed)
+        } else {
+            if(msg.content.toLowerCase() === "no")
+                message.channel.send('Cancelled command.')
+        }
+    })
     
     if (!message.member.hasPermission(["MANAGE_CHANNELS", "ADMINISTRATOR"])) return message.channel.send("You do not have permissions to perform this command").then(m => m.delete({ timeout: 5000}));
 
@@ -35,19 +47,5 @@ module.exports = class extends BaseCommand {
       .setDescription(`:tick: Set the slowmode of this channel to **${args[0]}** with the reason: **${reason}**`)
       .setFooter(`Done by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
       .setTimestamp()
-
-      message.channel.send(embed)
-
-const filter = (m) => m.author.id === message.author.id && (m.content.toLowerCase() === "yes" || m.content.toLowerCase() === "no")  // Create a filter, only accept messages from the user that used the command and the message includes "yes" or "no"
-message.channel.awaitMessages(filter, {max: 1, time: 30000})
-    .then(collected => {
-        const msg = collected.first()
-        if(msg.content.toLowerCase() === "yes") {
-            message.channel.send(embed)
-        } else {
-            if(msg.content.toLowerCase() === "no")
-                message.channel.send('Cancelled command.')
-        }
-    })
   }
   }
