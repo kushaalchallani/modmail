5e69efc2f46f92e34e8a7154fe0d0474e556e201
Kushaal Challani <66863823+kcplayz@users.noreply.github.com>
2020-07-26 18:08:44 +0530

    Update slowmode.js

--- src/commands/Moderation/slowmode.js
+++ src/commands/Moderation/slowmode.js
@@ -15,13 +15,6 @@ module.exports = class extends BaseCommand {
 
     if (!message.member.hasPermission(["MANAGE_CHANNELS", "ADMINISTRATOR"])) return message.channel.send("You do not have permissions to perform this command").then(m => m.delete({ timeout: 5000}));
 
-    const embed = new Discord.MessageEmbed()
-    .setColor('GREEN')
-    .setTitle('Slowmode')
-    .setDescription(`:tick: Set the slowmode of this channel to **${args[0]}** with the reason: **${reason}**`)
-    .setFooter(`Done by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-    .setTimestamp()
-
     if (!args[0])
     return message.channel.send(
       `You did not specify the time in seconds you wish to set this channel's slow mode too!`
@@ -33,6 +26,13 @@ module.exports = class extends BaseCommand {
   if (!reason) {
     reason == "No reason provided!";
   }
+
+  const embed = new Discord.MessageEmbed()
+  .setColor('GREEN')
+  .setTitle('Slowmode')
+  .setDescription(`:tick: Set the slowmode of this channel to **${args[0]}** with the reason: **${reason}**`)
+  .setFooter(`Done by ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+  .setTimestamp()
       
     const filter = (m) => m.author.id === message.author.id && (m.content.toLowerCase() === "yes" || m.content.toLowerCase() === "no")  // Create a filter, only accept messages from the user that used the command and the message includes "yes" or "no"
     message.channel.awaitMessages(filter, {max: 1, time: 30000})
